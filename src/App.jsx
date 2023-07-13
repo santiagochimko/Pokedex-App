9dfad811a39ad9c16bf4bf4094f9e224616a1ec2
Santiago Chimko <santiagochimko@gmail.com>
2023-07-13 10:53:01 -0300

    llamado a la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [pokemones, setPokemones] = useState([]);
+  useEffect(() => {
+    const getPokemones = async () => {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+      );
+      const listaPokemones = await response.json();
+      const { results } = listaPokemones;
+      
+      const newPokemones = results.map(async (pokemon) => {
+       
+        const response = await fetch((pokemon).url)
+        const poke = await response.json()
+      
+        return{
+          id: poke.id,
+          name: poke.name,
+          img: poke.sprites.other.dream_world.front_default
+        }
+      })
+      setPokemones(await Promise.all(newPokemones))
+    };
+
+    getPokemones();
+  }, []);
 
   return (
-    <div className='App'>
+    <>
+    <div className="App">
       <h1>Pokédex</h1>
+      {
+        pokemones.map(pokemon => {
+          return(
+            <div>
+              <img src={pokemon.img} alt={pokemon.name} />
+              <p>{pokemon.name}</p>
+              <span>{pokemon.id}</span>
+              </div>
+          )
+        })
+      }
     </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
