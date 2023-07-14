b97290fce5e5828c0cbc76822f6458cde965fcdb
Santiago Chimko <santiagochimko@gmail.com>
2023-07-14 10:42:49 -0300

    router main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="pokemons" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
 )
