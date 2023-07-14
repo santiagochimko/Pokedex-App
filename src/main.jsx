bdfa74d097fb247c4c1300cdd59788f7c47555a8
Jhonatan Leyton <totambo@gmail.com>
2023-07-14 10:52:20 -0300

    cambios modal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Route, Routes, BrowserRouter} from "react-router-dom"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="pokemons" element={<App />} />
+      </Routes>
+    </BrowserRouter> 
+  </React.StrictMode>
 )
