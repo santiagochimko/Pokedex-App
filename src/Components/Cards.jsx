eda4b4bedbbfb98168e85ebf9545526274569ea0
Santiago Chimko <santiagochimko@gmail.com>
2023-07-17 11:01:16 -0300

    obtener colores

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,6 +2,29 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Cards.css";
 
+export const colorsByType = {
+  normal: { borderColor: "#AAA67F", backgroundColor: "#AAA67F", color: "#AAA67F" },
+  fighting: { borderColor: "#C12239", backgroundColor: "#C12239", color: "#C12239" },
+  flying: { borderColor: "#A891EC", backgroundColor: "#A891EC", color: "#A891EC" },
+  poison: { borderColor: "#A43E9E", backgroundColor: "#A43E9E", color: "#A43E9E" },
+  ground: { borderColor: "#DEC16B", backgroundColor: "#DEC16B", color: "#DEC16B" },
+  rock: { borderColor: "#B69E31", backgroundColor: "#B69E31", color: "#B69E31" },
+  bug: { borderColor: "#A7B723", backgroundColor: "#A7B723", color: "#A7B723" },
+  ghost: { borderColor: "#70559B", backgroundColor: "#70559B", color: "#70559B" },
+  steel: { borderColor: "#B7B9D0", backgroundColor: "#B7B9D0", color: "#B7B9D0" },
+  fire: { borderColor: "#F57D31", backgroundColor: "#F57D31", color: "#F57D31" },
+  water: { borderColor: "#6493EB", backgroundColor: "#6493EB", color: "#6493EB" },
+  grass: { borderColor: "#74CB48", backgroundColor: "#74CB48", color: "#74CB48" },
+  electric: { borderColor: "#F9CF30", backgroundColor: "#F9CF30", color: "#F9CF30" },
+  psychic: { borderColor: "#F85888", backgroundColor: "#FA92B2", color: "#F85888" },
+  ice: { borderColor: "#9AD6DF", backgroundColor: "#9AD6DF", color: "#9AD6DF" },
+  dragon: { borderColor: "#7037FF", backgroundColor: "#7037FF", color: "#7037FF" },
+  dark: { borderColor: "#75574C", backgroundColor: "#75574C", color: "#75574C" },
+  fairy: { borderColor: "#E69EAC", backgroundColor: "#E69EAC", color: "#E69EAC" },
+  unknown: { borderColor: "#68A090", backgroundColor: "#68A090", color: "#68A090" },
+  shadow: { borderColor: "#444444", backgroundColor: "#444444", color: "#444444" },
+};
+
 function Cards({ filterValue, sortBy }) {
   const [pokemones, setPokemones] = useState([]);
 
@@ -34,28 +57,7 @@ function Cards({ filterValue, sortBy }) {
     getPokemones();
   }, []);
 
-  const colorsByType = {
-    normal: { borderColor: "#AAA67F", backgroundColor: "#AAA67F", color: "#AAA67F" },
-    fighting: { borderColor: "#C12239", backgroundColor: "#C12239", color: "#C12239" },
-    flying: { borderColor: "#A891EC", backgroundColor: "#A891EC", color: "#A891EC" },
-    poison: { borderColor: "#A43E9E", backgroundColor: "#A43E9E", color: "#A43E9E" },
-    ground: { borderColor: "#DEC16B", backgroundColor: "#DEC16B", color: "#DEC16B" },
-    rock: { borderColor: "#B69E31", backgroundColor: "#B69E31", color: "#B69E31" },
-    bug: { borderColor: "#A7B723", backgroundColor: "#A7B723", color: "#A7B723" },
-    ghost: { borderColor: "#70559B", backgroundColor: "#70559B", color: "#70559B" },
-    steel: { borderColor: "#B7B9D0", backgroundColor: "#B7B9D0", color: "#B7B9D0" },
-    fire: { borderColor: "#F57D31", backgroundColor: "#F57D31", color: "#F57D31" },
-    water: { borderColor: "#6493EB", backgroundColor: "#6493EB", color: "#6493EB" },
-    grass: { borderColor: "#74CB48", backgroundColor: "#74CB48", color: "#74CB48" },
-    electric: { borderColor: "#F9CF30", backgroundColor: "#F9CF30", color: "#F9CF30" },
-    psychic: { borderColor: "#F85888", backgroundColor: "#FA92B2", color: "#F85888" },
-    ice: { borderColor: "#9AD6DF", backgroundColor: "#9AD6DF", color: "#9AD6DF" },
-    dragon: { borderColor: "#7037FF", backgroundColor: "#7037FF", color: "#7037FF" },
-    dark: { borderColor: "#75574C", backgroundColor: "#75574C", color: "#75574C" },
-    fairy: { borderColor: "#E69EAC", backgroundColor: "#E69EAC", color: "#E69EAC" },
-    unknown: { borderColor: "#68A090", backgroundColor: "#68A090", color: "#68A090" },
-    shadow: { borderColor: "#444444", backgroundColor: "#444444", color: "#444444" },
-  };
+  
 
   const filteredPokemones = pokemones.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(filterValue.toLowerCase())
--- src/Components/ExpandedCard.jsx
+++ src/Components/ExpandedCard.jsx
@@ -3,6 +3,7 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import "./ExpandedCard.css";
 import arrowLeftImage from "../Images/arrow-left.svg";
 import arrowRightImage from "../Images/arrow-right.svg";
+import { colorsByType } from "./Cards";
 
 const ExpandedCard = () => {
   const { id } = useParams();
@@ -35,39 +36,66 @@ const ExpandedCard = () => {
     navigate(`/pokemons/${nextId}`);
   };
 
+  const getTypeColor = (type) => {
+    const typeData = colorsByType[type];
+    if (typeData) {
+      return {
+        borderColor: typeData.borderColor,
+        backgroundColor: typeData.backgroundColor,
+        color: typeData.color,
+      };
+    }
+    return {};
+  };
+
   return (
-    <div className="expandedCardContainer">
-      <div className="expandedCard">
+    <div className="expandedCardContainer" >
+      <div className="expandedCard" style={{
+      backgroundColor: getTypeColor(types[0].type.name).backgroundColor,
+    }}>
         <div className="expandedCardHeader">
           <div>
-          <Link to="/">
-            <button className="closeButton"><img src={arrowLeftImage} alt="close modal" /></button>
-          </Link>
-          <h2>{name}</h2>
+            <Link to="/">
+              <button className="closeButton">
+                <img src={arrowLeftImage} alt="close modal" />
+              </button>
+            </Link>
+            <h2>{name}</h2>
           </div>
           <p>#{id}</p>
         </div>
         <div className="imagePosition">
-        {id !== "1" && (
-          <button className="navButton" onClick={handlePreviousCard}>
-            <img src={arrowLeftImage} alt="Previous" />
+          {id !== "1" && (
+            <button className="navButton" onClick={handlePreviousCard}>
+              <img src={arrowLeftImage} alt="Previous" />
+            </button>
+          )}
+          <img src={sprites.other.dream_world.front_default} alt={name} />
+          <button className="navButton" onClick={handleNextCard}>
+            <img src={arrowRightImage} alt="Next" />
           </button>
-        )}
-        <img src={sprites.other.dream_world.front_default} alt={name} />
-        <button className="navButton" onClick={handleNextCard}>
-          <img src={arrowRightImage} alt="Next" />
-        </button>
         </div>
-        
+
         <p>Height: {height}</p>
         <p>Weight: {weight}</p>
-        <p>Types: {types.map((type) => type.type.name).join(", ")}</p>
+        <p>
+          Types:{" "}
+          {types.map((type) => (
+            <span
+              key={type.type.name}
+              style={{backgroundColor:getTypeColor(type.type.name)}}
+              className="type"
+            >
+              {type.type.name}
+            </span>
+          ))}
+        </p>
         <p>Stats:</p>
         <div className="stats">
           <ul className="statsLi">
             {stats.map((stat) => (
               <li key={stat.stat.name} className="liStats">
-                <span>
+                <span style={getTypeColor(stat.stat.name)}>
                   {stat.stat.name}: {stat.base_stat}
                 </span>
                 <div key={stat.base_stat} className="statBarFull">
@@ -79,7 +107,6 @@ const ExpandedCard = () => {
               </li>
             ))}
           </ul>
-          <div></div>
         </div>
       </div>
     </div>
